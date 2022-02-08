898f4f410c9474d21722b82bb83a137efdf294a4
omcewan <orlandomcewan231@gmail.com>
2022-02-08 18:22:14 -0500

    set up the Player constructor and added methods by using the prototype keyword

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,7 +1,7 @@
 const Player = require("../lib/Player");
 const Potion = require("../lib/Potion");
 jest.mock("../lib/Potion");
-console.log(new Potion())
+console.log(new Potion());
 
 test("crates a player object", () => {
   const player = new Player("Dave");
@@ -13,4 +13,23 @@ test("crates a player object", () => {
   expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
   );
+});
+
+test("gets player's stats as an object", () => {
+  const player = new Player("Dave");
+
+  expect(player.getStats()).toHaveProperty("potions");
+  expect(player.getStats()).toHaveProperty("health");
+  expect(player.getStats()).toHaveProperty("strength");
+  expect(player.getStats()).toHaveProperty("agility");
+});
+
+test("gets inventory from player or returns false", () => {
+  const player = new Player("Dave");
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
 });
